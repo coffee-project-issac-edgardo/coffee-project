ba2b52d476545c29794b5bacd94d27829cb296af
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-01-10 11:42:09 -0600

    coffee progress submission

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 function renderCoffee(coffee) {
     let html = '<div class="coffee">';
-    html += '<p>' + coffee.id + '</p>';
     html += '<p>' + coffee.name + '</p>';
     html += '<p>' + coffee.roast + '</p>';
     html += '</div>';
@@ -21,6 +20,7 @@ function renderCoffees(coffees) {
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roastSelection.value;
+    console.log(selectedRoast);
     let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
@@ -32,20 +32,20 @@ function updateCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    { name: 'Light City', roast: 'light'},
-    { name: 'Half City', roast: 'light'},
-    { name: 'Cinnamon', roast: 'light'},
-    { name: 'City', roast: 'medium'},
-    { name: 'American', roast: 'medium'},
-    { name: 'Breakfast', roast: 'medium'},
-    { name: 'High', roast: 'dark'},
-    { name: 'Continental', roast: 'dark'},
-    { name: 'New Orleans', roast: 'dark'},
-    { name: 'European', roast: 'dark'},
-    { name: 'Espresso', roast: 'dark'},
-    { name: 'Viennese', roast: 'dark'},
-    { name: 'Italian', roast: 'dark'},
-    { name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
 ];
 
 let tbody = document.querySelector('#coffees');
